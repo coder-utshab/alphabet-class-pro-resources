45e8a4efb9242cc1946de48b60eb7baaa1ff5f58
coder-utshab <ghoshutshob470@gmail.com>
2026-01-20 01:40:50 +0530

    min

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,12 @@
 function play(){
-    console.log('play start now');
+    // step-1: hide the home screen.to hide the screen add the class hidden to the home screen.
+const homeSection = document.getElementById('home-screen');
+homeSection.classList.add('hidden');
+// console.log(homeSection.classList)
+
+    //show the playground
+const playgroundSection = document.getElementById('play-ground');
+playgroundSection.classList.remove('hidden');
+// console.log(playgroundSection.classList)
+
 }
