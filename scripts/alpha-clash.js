687a0388645a1ef47d1dc55260c1ca2fb4b2fbd6
coder-utshab <ghoshutshob470@gmail.com>
2026-01-20 11:00:01 +0530

    pl

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,12 +1,19 @@
+// function play(){
+//     // step-1: hide the home screen.to hide the screen add the class hidden to the home screen.
+// const homeSection = document.getElementById('home-screen');
+// homeSection.classList.add('hidden');
+// // console.log(homeSection.classList)
+
+//     //show the playground
+// const playgroundSection = document.getElementById('play-ground');
+// playgroundSection.classList.remove('hidden');
+// // console.log(playgroundSection.classList)
+
+// }
+
+
 function play(){
-    // step-1: hide the home screen.to hide the screen add the class hidden to the home screen.
-const homeSection = document.getElementById('home-screen');
-homeSection.classList.add('hidden');
-// console.log(homeSection.classList)
-
-    //show the playground
-const playgroundSection = document.getElementById('play-ground');
-playgroundSection.classList.remove('hidden');
-// console.log(playgroundSection.classList)
-
+    hideElementById('home-screen');
+    showElementById('play-ground')
 }
+
