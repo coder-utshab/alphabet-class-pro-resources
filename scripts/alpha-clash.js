e835e85fa7369a94fa1d42da60fbc00d952f456a
coder-utshab <ghoshutshob470@gmail.com>
2026-01-21 21:42:08 +0530

    up

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,7 +11,9 @@
 
 // }
 function handleKeyboardKeyUpEvent(event){
-    const playerPressed = event.key;
+    // const playerPressed = event.key;
+    const playerPressed = event.key.toLowerCase();
+
 
     // key player is expected to press
    const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -19,20 +21,31 @@ function handleKeyboardKeyUpEvent(event){
    const expectedAlphabet = currentAlphabet.toLowerCase();
 
 //    check right or wrong key pressed
-if(playerPressed == expectedAlphabet){
+if(playerPressed == expectedAlphabet) {
     console.log('you got a point!');
+
+const currentScore = getTextElementValueById ('current-score');
+const updatedScore = currentScore + 1;
+setTextElementValueById('current-score', updatedScore);
+
+
+
+
+
+    //---------------------------------------------------------
     //update score
     //1. get the current score
-    const currentScoreElement = document.getElementById('current-score');
-    const currentScoreText = currentScoreElement.innerText;
-    const currentScore = parseInt(currentScoreText);
-    console.log(currentScoreText);
+    // const currentScoreElement = document.getElementById('current-score');
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+    // console.log(currentScore);
+    
 
-    //2. increse by the score by 1
+    // //2. increse by the score by 1
     const newScore = currentScore + 1;
 
-    //3. show the updated score
-    currentScoreElement.innerText = newScore;
+    // //3. show the updated score
+    // currentScoreElement.innerText = newScore;
 
     //start a new round
     removeBackgroundColorById(expectedAlphabet)
@@ -41,16 +54,16 @@ if(playerPressed == expectedAlphabet){
 else{
     console.log('dhurr vaiyaa va apu...right key press koro')
     //step 1 : get the current life number
-     const currentLifeElement = document.getElementById('current-life');
-     const currentLifeText = currentLifeElement.innerText;
-     const currentLife = parseInt(currentLifeText);
+    //  const currentLifeElement = document.getElementById('current-life');
+    //  const currentLifeText = currentLifeElement.innerText;
+    //  const currentLife = parseInt(currentLifeText);
 
-    //step 2: reduce the life count
-     const newLife = currentLife - 1;
+    // //step 2: reduce the life count
+    //  const newLife = currentLife - 1;
 
-    //step 3: display the updatedd life count
-    currentLifeElement.innerText = newLife;
-}
+    // //step 3: display the updatedd life count
+    // currentLifeElement.innerText = newLife;
+   }
 
 }
 
